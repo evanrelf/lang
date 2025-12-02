b728b8af34de373be536c77c79946ec2e4ad766d
Evan Relf <evan@evanrelf.com>
2025-12-01 22:48:13 -0800

    Environment has evaluated variables

--- src/Ecru/Eval.hs
+++ src/Ecru/Eval.hs
@@ -5,23 +5,26 @@ module Ecru.Eval
 where
 
 import Data.Map.Strict qualified as Map
+import Ecru.Lex (lex)
 import Ecru.Literal (Literal (..))
+import Ecru.Parse (parse)
 import Ecru.Syntax as Syntax (Syntax (..))
 import Ecru.Value as Value (Value (..))
 
-eval :: Map Text Syntax -> Syntax -> Value
+eval :: Map Text Value -> Syntax -> Value
 eval scope = \case
   Syntax.Literal lit -> Value.Literal lit
   Syntax.Variable var ->
     case Map.lookup var scope of
-      Just expr -> eval scope expr
+      Just val -> val
       Nothing -> Value.Variable var
   Syntax.Lambda param body -> Value.Lambda param body
   Syntax.Application fn arg -> apply scope fn arg
 
-apply :: Map Text Syntax -> Syntax -> Syntax -> Value
+apply :: Map Text Value -> Syntax -> Syntax -> Value
 apply scope = \cases
-  (Syntax.Lambda param body) arg -> eval (Map.insert param arg scope) body
+  (Syntax.Lambda param body) arg ->
+    eval (fix \final -> Map.insert param (eval final arg) scope) body
 
   -- Add integers
   (Syntax.Application (Syntax.Variable "add") (Syntax.Literal (Integer x)))
@@ -33,8 +36,14 @@ apply scope = \cases
 
   fn arg -> Value.Application (eval scope fn) (eval scope arg)
 
-prelude :: Map Text Syntax
+prelude :: Map Text Value
 prelude = Map.fromList
-  [ ("identity", [parsed|x: x|])
-  , ("const", [parsed|x: _: x|])
+  [ ("identity", expr "x: x")
+  , ("const", expr "x: _: x")
   ]
+  where
+  expr :: Text -> Value
+  expr = eval prelude . boldly parse . boldly lex
+
+  boldly :: (a -> Either Text b) -> (a -> b)
+  boldly f = either error identity . f
