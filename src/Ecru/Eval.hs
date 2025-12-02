c571631ae4229fad38f086e44946979053c4cde1
Evan Relf <evan@evanrelf.com>
2025-12-01 21:19:58 -0800

    Start tracking lexical scope

--- src/Ecru/Eval.hs
+++ src/Ecru/Eval.hs
@@ -3,25 +3,26 @@ module Ecru.Eval
   )
 where
 
+import Data.Map.Strict qualified as Map
 import Ecru.Literal (Literal (..))
 import Ecru.Syntax as Syntax (Syntax (..))
 import Ecru.Value as Value (Value (..))
 
-eval :: Syntax -> Value
-eval = \case
+eval :: Map Text Syntax -> Syntax -> Value
+eval scope = \case
   Syntax.Literal lit -> Value.Literal lit
   Syntax.Variable var -> Value.Variable var
-  Syntax.Lambda param body -> Value.Lambda param (eval body)
-  Syntax.Application fn arg -> apply fn arg
+  Syntax.Lambda param body -> Value.Lambda param (eval scope body)
+  Syntax.Application fn arg -> apply scope fn arg
 
-apply :: Syntax -> Syntax -> Value
-apply = \cases
+apply :: Map Text Syntax -> Syntax -> Syntax -> Value
+apply scope = \cases
   -- Identity function
-  (Syntax.Lambda param (Syntax.Variable var)) arg | param == var -> eval arg
+  (Syntax.Lambda param (Syntax.Variable var)) arg | param == var -> eval scope arg
 
   -- Const function
   (Syntax.Lambda param (Syntax.Lambda _ (Syntax.Variable var))) arg
-    | param == var -> eval arg
+    | param == var -> eval scope arg
 
   -- Add integers
   (Syntax.Application (Syntax.Variable "add") (Syntax.Literal (Integer x)))
@@ -31,4 +32,4 @@ apply = \cases
   (Syntax.Application (Syntax.Variable "add") (Syntax.Literal (Floating x)))
     (Syntax.Literal (Floating y)) -> Value.Literal (Floating (x + y))
 
-  fn arg -> Value.Application (eval fn) (eval arg)
+  fn arg -> Value.Application (eval scope fn) (eval scope arg)
